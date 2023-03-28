5fb614ec7ce4d052aea20172ea54aa740560d4ac
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-28 11:51:04 -0600

    finished simple server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/search', (req, res) => {
+    res.render('search')
+});
+
+app.listen(3000, () => {
+    console.log('server started at 3000')
+})
